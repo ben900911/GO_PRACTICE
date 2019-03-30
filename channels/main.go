e40970fd6648943affa7a4ee821e9d1aba661842
ben900911 <ben900911@gmail.com>
2019-03-31 00:35:50 +0800

    add channels

---
+++ channels/main.go
@@ -0,0 +1,15 @@
+package main
+
+func main() {
+	links := []string{
+		"http://gogle.com",
+		"http://facebook.com",
+		"http://stackoverflow.com",
+		"http://golang.org",
+		"http://amazon.com",
+	}
+
+	for _, link := range links {
+
+	}
+}
