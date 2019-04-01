d7f726b9445e52195e92d2d2a7af2fee1c658d90
ben900911 <ben900911@gmail.com>
2019-04-02 01:37:49 +0800

    try to add a new goroutine

--- channels/main.go
+++ channels/main.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
 func main() {
 	links := []string{
 		"http://gogle.com",
@@ -10,6 +15,20 @@ func main() {
 	}
 
 	for _, link := range links {
-
+		//block call
+		//checkLink(link)
+		//go + func  : run the func with new goroutine
+		go checkLink(link)
 	}
+}
+
+func checkLink(link string) {
+	_, err := http.Get(link)
+
+	if err != nil {
+		fmt.Println(link, " might be down!")
+		return
+	}
+
+	fmt.Println(link, " is up!")
 }
