34c8e1f6f4a3f71fd0715d13f8fc75b6a1ea799a
ben900911 <ben900911@gmail.com>
2019-04-03 01:46:52 +0800

    add channel

--- channels/main.go
+++ channels/main.go
@@ -14,21 +14,28 @@ func main() {
 		"http://amazon.com",
 	}
 
+	c := make(chan string)
+
 	for _, link := range links {
 		//block call
 		//checkLink(link)
 		//go + func  : run the func with new goroutine
-		go checkLink(link)
+		go checkLink(link, c)
+	}
+
+	for L := range c {
+		go checkLink(L, c)
 	}
 }
 
-func checkLink(link string) {
+func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 
 	if err != nil {
 		fmt.Println(link, " might be down!")
+		c <- link
 		return
 	}
-
 	fmt.Println(link, " is up!")
+	c <- link
 }
